992027bb0c7d0203797277bf767b4d50fc73a312
Aagii <aagiiaga631@gmail.com>
2022-04-18 15:49:14 +0800

    Event listneruudiig uusgej ugluu

---
+++ app.js
@@ -0,0 +1,25 @@
+// дэлгэцтэй ажиллах контроллер
+var uiController = (function () {})();
+
+// санхүүтэй ажиллах контроллер
+var financeController = (function () {})();
+
+// Програмын холбогч контроллер
+var appController = (function (uiController, financeController) {
+  var ctrlAddItem = function () {
+    // 1. Оруулах өгөгдлийг дэлгэцээс олж авна
+    // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
+    // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана.
+    // 4. Төсвийг тооцоолно.
+    // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
+  };
+  document.querySelector(".add__btn").addEventListener("click", function () {
+    ctrlAddItem();
+  });
+
+  document.addEventListener("keypress", function (event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
+})(uiController, financeController);
